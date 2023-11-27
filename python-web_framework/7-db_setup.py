a53175d60c96d88c4e322d4de1ab344ef23fb959
ehoneahobed <ehoneahobed@hotmail.com>
2023-11-27 16:08:12 -0500

    Solution to task 1

--- python-web_framework/7-db_setup.py
+++ python-web_framework/7-db_setup.py
@@ -27,6 +27,8 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
 #################################################################
 
